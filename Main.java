4b791f77e251f066d4a2cdadf2b6ff71c451313f
Sergio Quesada <sqada2804@gmail.com>
2024-04-26 18:17:48 -0600

    Declaracion de Variables

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int num3;
+        double num4;
+        double num5;
+        double result;
+    }
+}
